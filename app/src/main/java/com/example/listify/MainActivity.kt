8fe7e80f294309e6e0173acd5f300c74301e0a01
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-07-24 14:23:01 +0700

    Add : add color scheme light and darj

--- app/src/main/java/com/example/listify/MainActivity.kt
+++ app/src/main/java/com/example/listify/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,10 +18,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ListifyTheme {
-                Box(modifier = Modifier.fillMaxSize()){
+            ListifyTheme(dynamicColor = false) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                ){
                     Text(
                         text = "Listify App",
+                        color = MaterialTheme.colorScheme.onPrimary,
                         modifier = Modifier.align(Alignment.Center)
                     )
                 }
--- app/src/main/java/com/example/listify/presentation/theme/Color.kt
+++ app/src/main/java/com/example/listify/presentation/theme/Color.kt
@@ -2,10 +2,8 @@ package com.example.listify.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val primary = Color(0xff03346e)
+val secondary = Color(0xFF0051ae)
+val tertiary = Color(0xFF005adf)
+val light = Color(0xFFf6f8fc)
+val dark = Color(0xFF0D1139)
